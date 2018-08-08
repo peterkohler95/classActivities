1065ef086ca3f3f0c4c3f14f20a6629d02cc3e5b
Peter K <peterkohler95@gmail.com>
2018-08-08 12:37:28 -0500

    solved customer-object.html

--- Coursework/week-3/day-3/Activities/99-other-activities/functions.js
+++ Coursework/week-3/day-3/Activities/99-other-activities/functions.js
@@ -14,3 +14,25 @@ function reverse(word) {
 }
 
 console.log(reverse("Peter"));
+
+// Bubble sort
+var myArray = [3, 1, 3, 6, 2, 9, 4, 5];
+
+function bubbleSort(array) {
+    var sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                sorted = false;
+                var temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSort(myArray));
+
