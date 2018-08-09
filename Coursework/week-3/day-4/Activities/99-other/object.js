757601d83fd8d4f892252f0c1a46fcafc035405f
Peter K <peterkohler95@gmail.com>
2018-08-09 12:49:04 -0500

    solved bujumbura-easier.html

--- Coursework/week-3/day-4/Activities/99-other/object.js
+++ Coursework/week-3/day-4/Activities/99-other/object.js
@@ -18,3 +18,36 @@ var object = {
 x = object.address.street.subdivision.board.members[3];
 
 console.log(x);
+
+var object = {
+    address: {
+        street: {
+            number: 123,
+            name: "Elm Street",
+            subdivision: {
+                name: "Nightmares Only",
+                board: {
+                    members: [
+                        "Freddy", "Jason", "Michael", "Jamie"
+                    ]
+                }
+            }
+        }
+    },
+    homes: [
+        {
+            type: "Mansion",
+            contractors: [
+                {
+                    name: "Joan Plumbing Co."
+                },
+                {
+                    name: "Suzanne Electic Boutique"
+                },
+            ]
+        }
+    ]
+}
+y = object.homes[0].contractors[1].name;
+console.log(y);
+
