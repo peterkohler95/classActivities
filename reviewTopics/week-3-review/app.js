c4b88c0a2dad7d6cab95eeda5aa716fdced56032
Peter K <peterkohler95@gmail.com>
2018-08-10 10:13:27 -0500

    added week 3 review and completed hangman-game

---
+++ reviewTopics/week-3-review/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    // get access to the button and add a click listener
+    $("button").on("click", function (event) {
+        event.preventDefault();
+
+        // capture the input
+        var studentName = $("input").val();
+
+        // append the input to the screen
+        // create tr
+        var tr = $("<tr>");
+        // create td
+        var td = $("<td>");
+        // set the contents of the td
+        td.text(studentName);
+
+        // append the td to the tr
+        tr.append(td);
+        // append the tr to the screen
+        $("table").append(tr);
+        // clear the input?
+        $("input").val("");
+    });
+
+
+});
+
