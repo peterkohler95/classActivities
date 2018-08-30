207e8f9ccd1f1b337d73898b06fe38fc96b7a91e
Peter K <peterkohler95@gmail.com>
2018-08-30 11:16:07 -0500

    solved callbacks.js

--- Coursework/week-6/day-2/Activities/15-Callbacks/Unsolved/callbacks.js
+++ Coursework/week-6/day-2/Activities/15-Callbacks/Unsolved/callbacks.js
@@ -2,14 +2,36 @@ var fs = require("fs");
 
 // Write a function that logs a message, then executes
 // a function argument.
+function logMes(str, cb) {
+    console.log(str);
+    cb();
+}
 
 // Write a function that runs a function argument if
 // its other argument is truthy.
-
+function truthy(bool, cb) {
+    if (bool) {
+        cb();
+    }
+}
 // Write a function that accepts a function argument and
 // a value, and returns a function that returns the result
 // of executing the function argument with the value.
 // This isn't as hard as it sounds!
+function func(f, v) {
+    return function () {
+        return f(v);
+    }
+}
+
+function sum(a) {
+    return function (b) {
+        return a + b;
+    }
+}
+
+var x = sum(5)(10);
+console.log(x);
 
 // Use fs.writeFile to log a message to a file called
 // log.txt. Are yo using callbacks anywhere? Where?
