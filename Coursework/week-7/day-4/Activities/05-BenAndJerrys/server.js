63567535e00d88e4a42c1572e05828c8bb8cd7e5
Peter K <peterkohler95@gmail.com>
2018-09-14 10:32:19 -0500

    added .gitignore

--- Coursework/week-7/day-4/Activities/05-BenAndJerrys/server.js
+++ Coursework/week-7/day-4/Activities/05-BenAndJerrys/server.js
@@ -19,7 +19,8 @@ var icecreams = [
     { name: 'chocolate', price: 4, awesomeness: 8 },
     { name: 'banana', price: 1, awesomeness: 1 },
     { name: 'greentea', price: 5, awesomeness: 7 },
-    { name: 'jawbreakers', price: 6, awesomeness: 2 },
+    { name: 'jawbreakers', price: 6, awesomeness: 2 }
+
 ];
 
 // Routes
