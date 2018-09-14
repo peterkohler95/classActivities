b593fab42465a09766b7a5d697fe66398ce4b31a
Peter K <peterkohler95@gmail.com>
2018-09-14 11:21:13 -0500

    added JAWSDB to connection.js

--- Coursework/week-7/day-5/Activities/17-CatsApp/Unsolved/config/connection.js
+++ Coursework/week-7/day-5/Activities/17-CatsApp/Unsolved/config/connection.js
@@ -1,23 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  database: "cat_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
 
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
